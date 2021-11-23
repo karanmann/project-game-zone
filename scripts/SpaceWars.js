35cead58e5e991a3831640262b07704248cb25f0
Karan Mann <karan.mann@outlook.com>
2021-11-23 21:09:18 +0100

    bugged code

--- scripts/SpaceWars.js
+++ scripts/SpaceWars.js
@@ -1,7 +1,7 @@
-let center, bg, playerShip, enemyShip, playerShipControls, startGame, text
+let center, bg, playerShip, enemyShip, playerShipControls, startGame, text,
 
-class SpaceWars extends Phaser.Scene {
-  preload() {
+class SpaceWars extends Phaser.Scene{
+  preload(){
     //GAME ASSETS
     this.load.image("playerShip", "../assets/ships/rebelShip.svg");
     this.load.image("enemyShip", "../assets/ships/PodShip.svg");
@@ -17,22 +17,13 @@ class SpaceWars extends Phaser.Scene {
     bg = this.add.image(center.x, center.y, "background");
     bg.setDisplaySize(center.x * 2, center.y * 2);
 
-    playerShip = this.physics.add.sprite(
-      window.innerWidth / 2,
-      window.innerHeight - 50,
-      "playerShip"
-    );
-
-    playerShip.setScale(0.3);
+    playerShip = this.physics.add.sprite(window.innerWidth / 2, window.innerHeight - 50, "playerShip");
+    playerShip.setScale(0.5);
     playerShip.setCollideWorldBounds(true); //Stops playerShip from leaving the frame.
     playerShipControls = this.input.keyboard.createCursorKeys() //Gives us access to Arrowkeys + Space + Shift
 
-    enemyShip = this.physics.add.sprite(
-      window.innerWidth / 2,
-      window.innerHeight / 5,
-      "enemyShip"
-    );
-    enemyShip.setScale(0.2);
+    enemyShip = this.physics.add.sprite( window.innerWidth / 2, window.innerHeight / 5, "enemyShip");
+    enemyShip.setScale(0.5);
     enemyShip.setFlipY(true) //To flip the image on its Y-axis. It can also be used on the X-axis
 
     text = this.add.text(center.x + 200, 10, "SCORE : XXX", {
@@ -43,7 +34,7 @@ class SpaceWars extends Phaser.Scene {
     text.setOrigin(0.5, 0);
   }
 
-  update() {
+  update(){
 
     if (playerShipControls.space.isDown) {
       startGame = true;
@@ -53,15 +44,15 @@ class SpaceWars extends Phaser.Scene {
       if (playerShipControls.left.isDown) {
         playerShip.setVelocity(-200, 0); //Left arrow is pressed
       }
-      if (playerShipControls.right.isDown) {
+      else if (playerShipControls.right.isDown) {
         playerShip.setVelocity(200, 0);//Right arrow is pressed
       }
-      if (playerShipControls.up.isDown) {
-        playerShip.setVelocity(0, -200);
-      }
-      if (playerShipControls.down.isDown) {
-        playerShip.setVelocity(0, 200);
-      }
+      // if (playerShipControls.up.isDown) {
+      //   playerShip.setVelocity(0, -200);
+      // }
+      // if (playerShipControls.down.isDown) {
+      //   playerShip.setVelocity(0, 200);
+      // }
     }
   }
 }
