d911a2dfbcb24003e09f78ba25bf0044a69044b0
Karan Mann <62376840+karanmann@users.noreply.github.com>
2021-11-25 13:42:56 +0100

    Collider and missed scores added

--- scripts/SpaceWars.js
+++ scripts/SpaceWars.js
@@ -1,6 +1,13 @@
-let center, bg, playerShip, enemyShip, playerShipControls, startGame, text
+let center,
+  bg,
+  playerShip,
+  enemyShip,
+  playerShipControls,
+  startGame,
+  text,
+  platform;
 
-const gameState = {}
+const gameState = { score: 0 };
 
 class SpaceWars extends Phaser.Scene {
   preload() {
@@ -8,74 +15,66 @@ class SpaceWars extends Phaser.Scene {
     this.load.image("playerShip", "../assets/ships/rebelShip.svg");
     this.load.image("enemyShip", "../assets/ships/PodShip.svg");
     this.load.image("background", "../assets/startPage/bg.jpeg");
+    this.load.image("bottom", "../assets/background/blackRectangle.svg");
   }
 
   create() {
+    bg = this.add.image(400, 300, "background");
+    bg.setDisplaySize(800, 600);
 
-    center = {
-      x: this.physics.world.bounds.width / 2,
-      y: this.physics.world.bounds.height / 2,
-    };
+    platform = this.physics.add.staticGroup();
+    platform.create(0, 730, "bottom");
 
-    bg = this.add.image(center.x, center.y, "background");
-    bg.setDisplaySize(center.x * 2, center.y * 2);
-
-    playerShip = this.physics.add.sprite(
-      window.innerWidth / 2,
-      window.innerHeight - 50,
-      "playerShip"
-    );
+    playerShip = this.physics.add.sprite(400, 500, "playerShip");
 
     playerShip.setScale(0.4);
     playerShip.setCollideWorldBounds(true); //Stops playerShip from leaving the frame.
-    playerShipControls = this.input.keyboard.createCursorKeys() //Gives us access to Arrowkeys + Space + Shift
+    playerShipControls = this.input.keyboard.createCursorKeys(); //Gives us access to Arrowkeys + Space + Shift
 
-    enemyShip = this.physics.add.sprite(
-      window.innerWidth / 2,
-      window.innerHeight / 5,
-      "enemyShip"
-    ); 
+    this.physics.add.collider(playerShip, platform);
 
+    enemyShip = this.physics.add.sprite(700, 100, "enemyShip");
     enemyShip.setScale(0.3);
-    enemyShip.setFlipY(true) //To flip the image on its Y-axis. It can also be used on the X-axis
+    enemyShip.setFlipY(true); //To flip the image on its Y-axis. It can also be used on the X-axis
 
-    const fallingEnemies = this.physics.add.group();  // Creating a group to generate random ships in
-  
-    function enemyGen () {
+    const fallingEnemies = this.physics.add.group(); // Creating a group to generate random ships in
+
+    function enemyGen() {
       const XCoord = Math.random() * 600; // Generates random enemyship on the X-axis
-      fallingEnemies.create(XCoord, 0, 'enemyShip').setScale(0.4);
+      fallingEnemies.create(XCoord, 0, "enemyShip").setScale(0.4);
     }
 
     this.time.addEvent({
-      delay : 900,
-      callback: enemyGen,  // using to call the function enemyGen()
+      delay: 900,
+      callback: enemyGen, // using to call the function enemyGen()
       callbackScope: this,
-      loop: true
+      loop: true,
     });
 
-    text = this.add.text(center.x + 200, 10, "SCORE : XXX", {
+    this.physics.add.collider(fallingEnemies, platform, function (singleEnemy) {
+      singleEnemy.destroy();
+      gameState.score += 1;
+      gameState.scoreText.setText(`ENEMIES MISSED: ${gameState.score}`)
+    });
+
+    gameState.scoreText = this.add.text(300, 560, "ENEMIES MISSED : 0", {
       fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
       fontSize: 20,
     });
 
-    text.setOrigin(0.5, 0);
   }
 
   update() {
-
     if (playerShipControls.left.isDown) {
       playerShip.setVelocity(-400, 0); //Left arrow is pressed
-    }
-    else if (playerShipControls.right.isDown) {
-      playerShip.setVelocity(400, 0);//Right arrow is pressed
-    }
-    else if (playerShipControls.up.isDown) {
-      playerShip.setVelocity(0, -400);
-    }
-    else if (playerShipControls.down.isDown) {
-      playerShip.setVelocity(0, 400);
+    } else if (playerShipControls.right.isDown) {
+      playerShip.setVelocity(400, 0); //Right arrow is pressed
+    } else if (playerShipControls.up.isDown) {
+      playerShip.setVelocity(0, -400); // Up arrow is pressed
+    } else if (playerShipControls.down.isDown) {
+      playerShip.setVelocity(0, 400); // Down arrow is pressed
     } else {
-      playerShip.setVelocity(0, 0);
+      playerShip.setVelocity(0, 0); // if nothing is pressed velocity on x & y-axis to 0
     }
   }
 }
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,6 +14,7 @@ var config = {
       debug: true,
     },
   },
+  backgroundColor: "#fff",
   scene: [SpaceWars],
 };
 
